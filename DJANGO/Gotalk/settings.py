49bc993eb7b04230bfdfc38fe3ce1aa0d28e646f
wlgustlraaa <wlgustlraaa@gmail.com>
2023-03-19 19:56:13 +0900

    [Fix] Gotalk init

--- DJANGO/Gotalk/settings.py
+++ DJANGO/Gotalk/settings.py
@@ -11,18 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os, environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEBUG = True
+
+env = environ.Env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
