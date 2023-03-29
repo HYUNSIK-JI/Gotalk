d84a4bfd8b411c8b39e5557bc0de85cdc1f0c1be
wlgustlraaa <wlgustlraaa@gmail.com>
2023-03-29 20:01:31 +0900

    css

--- DJANGO/chat/consumers.py
+++ DJANGO/chat/consumers.py
@@ -1,13 +1,6 @@
-# chat/consumers.py
-import json
-import datetime
-
 from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth import get_user_model
+import json
 
-x = datetime.datetime.now()
-plus = datetime.timedelta(hours=9) 
-connected_user = []
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -18,78 +11,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
-        username = self.scope["user"].username
-        if username in connected_user:
-            pass
-        else:
-            connected_user.append(username)
-            message = "님이 입장하셨습니다"
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "chat_message",
-                    "message": message,
-                    "username": username,
-                    "connected_user": len(connected_user),
-                },
-            )
+
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-        username = self.scope["user"].username
-        if username not in connected_user:
-            pass
-        else:
-            connected_user.remove(username)
-            message = "님이 퇴장하셨습니다"
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "chat_message",
-                    "message": message,
-                    "username": username,
-                    "connected_user": len(connected_user),
-                },
-            )
+
+
     # Receive message from WebSocket
     async def receive(self, text_data):
-        user = self.scope["user"]
         text_data_json = json.loads(text_data)
-        print(text_data_json, 12)
-        date = x + plus
-        date = date.strftime("%m월 %d일 %H:%M")
         message = text_data_json["message"]
-        
-        context = {
-            "userid": user.pk,
-            "username": user.username,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "date": date,
-        }
+
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message, "context":context,}
+            self.room_group_name, {"type": "chat_message", "message": message}
         )
+
 
     # Receive message from room group
     async def chat_message(self, event):
-        user = self.scope["user"]
-        context = {
-            "userid": user.pk,
-            "username": user.username,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "connected_user": len(connected_user),
-            "date": x.strftime("%m월 %d일 %H:%M"),
-        }
         message = event["message"]
+
         # Send message to WebSocket
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "message": message,
-                    "context": context,
-                }
-            )
-        )
+        await self.send(text_data=json.dumps({"message": message}))
--- DJANGO/chat/views.py
+++ DJANGO/chat/views.py
@@ -1,22 +1,43 @@
 # chat/views.py
 from django.shortcuts import render, redirect
-from accounts.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 import datetime
+
 def index(request):
     return render(request, "chat/index.html")
 
+#@login_required
+#def room(request, room_name):
+#    user = request.user
+#    if user.is_authenticated:
+#        x = datetime.datetime.now()
+#        plus = datetime.timedelta(hours=9)
+#        date = x + plus
+#        date = str(date.strftime("%m월 %d일 %H:%M"))
+#        context = {
+#            "username": user.username,
+#            "date": date,
+#            "user_pk": user.pk,
+#        }
+#        return render(request, "chat/room.html", {"room_name": room_name, "context": context})
+#    else:
+#        return redirect(request.GET.get("next") or "accounts:login")
+
+@login_required
 def room(request, room_name):
-    if request.user.is_authenticated:
-        user = User.objects.get(pk = request.user.pk)
-        x = datetime.datetime.now()
-        plus = datetime.timedelta(hours=9)
-        date = x + plus
-        date = str(date.strftime("%m월 %d일 %H:%M"))
-        context = {
-            "username": user.username,
-            "date": date,
-            "user_pk": user.pk,
-        }
-        return render(request, "chat/room.html", {"room_name": room_name, "context": context})
+    user = request.user
+    # 로그인 검증 및 스터디원 여부 파악
+    if user.is_authenticated:
+        nickname = user.username
+        memberimg = 'https://dummyimage.com/150x150'
     else:
-        return redirect(request.GET.get("next") or "accounts:login")
+        messages.error(request, "로그인을 해주세요.")
+        return redirect("accounts:login")
+
+    context = {
+        "nickname": nickname,
+        "memberimg": memberimg,
+    }
+
+    return render(request, "chat/room.html", {"room_name": room_name, "context": context})
